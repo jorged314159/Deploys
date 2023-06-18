8e997455e7e04b9c3dce63e09e60492d3b1d0197
Jorge <jorged314159@gmail.com>
2023-06-18 14:22:47 -0600

    Subiendo aapp Node

---
+++ appNode/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(3000, () => {
+  console.log('Servidor Node.js iniciado en el puerto 3001');
+});
